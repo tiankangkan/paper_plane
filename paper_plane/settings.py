883f8c080711bd2941c524e4cee324e1a4722784
tiankangkan <tiankangkan@163.com>
2016-04-17 07:07:04 +0800

    log changes

--- paper_plane/settings.py
+++ paper_plane/settings.py
@@ -135,10 +135,20 @@ STATIC_URL = '/static/'
 
 LOGGING = {
     'version': 1,
-    'disable_existing_loggers': True,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s'
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+    },
     'handlers': {
         'console': {
+            'level': 'DEBUG',
             'class': 'logging.StreamHandler',
+            'formatter': 'simple'
         },
     },
     'loggers': {
