63d912d5778a41a007e648ce4e86ab4e99704ab3
tiankangkan <tiankangkan@163.com>
2016-04-21 01:55:08 +0800

    fixed

--- love_me/views.py
+++ love_me/views.py
@@ -50,31 +50,6 @@ def reply_to_paper_plane(request):
 
 
 @csrf_exempt
-def reply_to_paper_plane_sender_do_submit_old(request):
-    req = get_request_body(request)
-    log_msg = '%s: %s' % (MSG_LOVE_ME_SUBMIT, json.dumps(req, indent=4))
-    log_inst.info(log_msg)
-    extra_source_id = req.get('extra_source_id', None)
-    how_much_love = req.get('how_much_love', None)
-    when_begin_love = req.get('when_begin_love', None)
-    want_to_say = req.get('want_to_say', None)
-    pic_name = req.get('pic_name', "0001.jpg")
-    t_id = req.get('t_id', None)
-    t_id = int(t_id) if t_id else None
-    if not t_id:
-        return HttpResponse(json.dumps(dict(status='error', msg=u'未确定的网页来源...')))
-    content_dict = dict(
-        how_much_love=how_much_love,
-        when_begin_love=when_begin_love,
-        want_to_say=want_to_say,
-        pic_name=pic_name,
-        extra_source_id=extra_source_id
-    )
-    update_conversation_page_db(t_id=t_id, source_id=extra_source_id, content_dict=content_dict, is_read=True)
-    return HttpResponse(json.dumps(dict(status='success', msg=u'成功')))
-
-
-@csrf_exempt
 def reply_to_paper_plane_sender_do_submit(request):
     try:
         req = get_request_body(request)
@@ -192,9 +167,9 @@ def reply_text_message_contains_paper_plane(extra_source_id='34567890_alice'):
     content_dict = dict(extra_source_id=extra_source_id, pic_name=pic_name, role='sender')
     para_str = urllib.urlencode(content_dict)
     paper_plane_url = '%s?%s' % (paper_plane_url, para_str)
-    text = u'扔个纸飞机吧: %s' % paper_plane_url
+    text = u'扔个纸飞机吧: \n%s' % paper_plane_url
     return text
 
 
 if __name__ == '__main__':
-    reply_text_message_contains_paper_plane(1123)
+    reply_text_message_contains_paper_plane('1234')
