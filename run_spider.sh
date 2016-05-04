46ba84d7716f007fa853fc3d8efa11e7fb2ccd06
tiankangkan <tiankangkan@163.com>
2016-05-04 13:10:49 +0800

    crontab update

--- run_spider.sh
+++ run_spider.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
-# 0 0,12 * * * * /bin/sh /data/project/paper_plane/run_spider.sh
-# 0 0,12 * * * * /bin/sh /Users/kangtian/Documents/Master/paper_plane/run_spider.sh
+
+# 0 0,12 * * * /bin/sh /data/project/paper_plane/run_spider.sh
+# 0 0,12 * * * /bin/sh /Users/kangtian/Documents/Master/paper_plane/run_spider.sh
 
 
 cd /data/project/paper_plane/king_spider/
 for var in "LuoWangJournal" "LuoWangEssays" "YiKeEssays" ; do
     echo "scrapy crawl: $var"
-    scrapy crawl $var
+    /usr/local/bin/scrapy crawl $var
 done
 
